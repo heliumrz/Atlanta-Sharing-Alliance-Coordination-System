443a8c0b0ecffeeb4470e4faeee6477d36d17516
Zhan, Rui <heliumrz@gatech.edu>
2017-07-18 09:24:03 -0700

    Completed data seeding according to the guideline

--- Phase3/team022_p3_complete_v7.sql
+++ Phase3/team022_p3_complete_v7.sql
@@ -2,7 +2,7 @@
  * Phase 3 Schema File | CS6400 - Summer 2017 | Team 022
  */
 
-CREATE USER IF NOT EXISTS gatechuser@localhost IDENTIFIED BY 'gatech123';
+CREATE USER IF NOT EXISTS gatechUser@localhost IDENTIFIED BY 'gatech123';
 DROP DATABASE IF EXISTS `cs6400_su17_team022`; 
 SET default_storage_engine=InnoDB;
 SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -12,9 +12,9 @@ CREATE DATABASE IF NOT EXISTS cs6400_su17_team022
    DEFAULT COLLATE utf8mb4_unicode_ci;
 USE cs6400_su17_team022;
 
-GRANT SELECT, INSERT, UPDATE, DELETE, FILE ON *.* TO 'gatechuser'@'localhost';
-GRANT ALL PRIVILEGES ON `gatechuser`.* TO 'gatechuser'@'localhost';
-GRANT ALL PRIVILEGES ON `cs6400_su17_team022`.* TO 'gatechuser'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, FILE ON *.* TO 'gatechUser'@'localhost';
+GRANT ALL PRIVILEGES ON `gatechUser`.* TO 'gatechUser'@'localhost';
+GRANT ALL PRIVILEGES ON `cs6400_su17_team022`.* TO 'gatechUser'@'localhost';
 FLUSH PRIVILEGES;
 
 -- Tables 
@@ -59,11 +59,13 @@ CREATE TABLE Service (
    
 CREATE TABLE ClientService (
    FacilityId int(16) unsigned NOT NULL,
+   SiteId int(16) unsigned NOT NULL,
    FacilityName varchar(100) NOT NULL,
    EligibilityCondition varchar(200) NOT NULL,
    HoursOfOperation varchar(50) NOT NULL,
    PRIMARY KEY (FacilityId),
-   CONSTRAINT fk_ClientService_FacilityId_Service_FacilityId FOREIGN KEY (FacilityId) REFERENCES Service (FacilityId) ON DELETE CASCADE
+   CONSTRAINT fk_ClientService_FacilityId_Service_FacilityId FOREIGN KEY (FacilityId) REFERENCES Service (FacilityId) ON DELETE CASCADE,
+   CONSTRAINT fk_ClientServiec_SiteId_Site_SiteId FOREIGN KEY (SiteId) REFERENCES Site (SiteId) ON DELETE CASCADE
    );
 
 CREATE TABLE Shelter (
@@ -84,7 +86,8 @@ CREATE TABLE FoodPantry(
    
 CREATE TABLE SoupKitchen (
    FacilityId int(16) unsigned NOT NULL,
-   SeatCount int NOT NULL,
+   SeatAvail int NOT NULL,
+   SeatTotal int NOT NULL,
    PRIMARY KEY (FacilityId),
    CONSTRAINT fk_SoupKitchen_FacilityId_Service_FacilityId FOREIGN KEY (FacilityId) REFERENCES ClientService (FacilityId) ON DELETE CASCADE   
    );
@@ -92,15 +95,16 @@ CREATE TABLE SoupKitchen (
 CREATE TABLE FoodBank (
    FacilityId int(16) unsigned NOT NULL,
    PRIMARY KEY (FacilityId),
-   CONSTRAINT fk_FoodBank_FacilityId_Service_FacilityId FOREIGN KEY (FacilityId) REFERENCES ClientService (FacilityId) ON DELETE CASCADE   
+   CONSTRAINT fk_FoodBank_FacilityId_Service_FacilityId FOREIGN KEY (FacilityId) REFERENCES Service (FacilityId) ON DELETE CASCADE   
    );
 
 CREATE TABLE Item (
-   ItemId int(16) unsigned NOT NULL,
+   ItemId varchar(50) NOT NULL,
    Name varchar(50) NOT NULL,
    StorageType varchar(50) NOT NULL,
-   ExpirationDate varchar(50) NOT NULL,
-   ItemType varchar(50) NOT NULL,
+   ExpDate varchar(50) NOT NULL,
+   Category varchar(50) NOT NULL,
+   SubCategory varchar(50) NOT NULL,
    PRIMARY KEY (ItemId)
 );
 
@@ -140,7 +144,7 @@ CREATE TABLE SiteToService (
 
 CREATE TABLE FoodBankToItem (
    FacilityId int(16) unsigned NOT NULL,
-   ItemId int(16) unsigned NOT NULL,
+   ItemId varchar(50) NOT NULL,
    AvailableQuantity int(10) NOT NULL,
    UNIQUE(FacilityId, ItemId),
    CONSTRAINT fk_FoodBankToItem_FacilityId_FoodBank_FacilityId FOREIGN KEY (FacilityId) REFERENCES FoodBank (FacilityId) ON DELETE CASCADE,
@@ -151,7 +155,7 @@ CREATE TABLE Request (
    RequestID int(16) unsigned NOT NULL AUTO_INCREMENT,
    Username varchar(30) NOT NULL,
    FacilityId int(16) unsigned NOT NULL,
-   ItemId int(16) unsigned NOT NULL,
+   ItemId varchar(50) NOT NULL,
    Status varchar(50) NOT NULL,
    QuantityRequested int NOT NULL,
    QuantityFulfilled int NOT NULL,
@@ -161,19 +165,6 @@ CREATE TABLE Request (
    CONSTRAINT fk_Request_ItemId_Item_ItemId FOREIGN KEY (ItemId) REFERENCES Item (ItemId) ON DELETE CASCADE
 );
 
-CREATE TABLE Food (
-   ItemId int(16) unsigned NOT NULL,
-   FoodCategory varchar(50) NOT NULL,
-   PRIMARY KEY (ItemId),
-   CONSTRAINT fk_Food_ItemId_Item_ItemId FOREIGN KEY (ItemId) REFERENCES Item (ItemId) ON DELETE CASCADE
-);
-
-CREATE TABLE Supply (
-   ItemId int(16) unsigned NOT NULL,
-   SupplyCategory varchar(50) NOT NULL,
-   PRIMARY KEY (ItemId),
-   CONSTRAINT fk_Supply_ItemId_Item_ItemId FOREIGN KEY (ItemId) REFERENCES Item (ItemId) ON DELETE CASCADE
-);
 
 /*
  * Phase 3 Demo Data File | CS6400 - Summer 2017 | Team 022
@@ -277,3 +268,230 @@ select * from User;
 select * from Client;
 select * from ClientLog;
 
+-- Inserting 12 Clients' phone number --
+UPDATE Client SET PhoneNumber='1-300-738-3425' WHERE ClientId='1';
+UPDATE Client SET PhoneNumber='189-582-8672' WHERE ClientId='2';
+UPDATE Client SET PhoneNumber='1-846-970-2514x674' WHERE ClientId='3';
+UPDATE Client SET PhoneNumber='1-956-852-3137' WHERE ClientId='4';
+UPDATE Client SET PhoneNumber='344-314-6224x35864' WHERE ClientId='5';
+UPDATE Client SET PhoneNumber='(146)580-0101x239' WHERE ClientId='6';
+UPDATE Client SET PhoneNumber='(724)357-5470' WHERE ClientId='7';
+UPDATE Client SET PhoneNumber='(380)537-9608' WHERE ClientId='8';
+UPDATE Client SET PhoneNumber='(715)152-7932x59008' WHERE ClientId='9';
+UPDATE Client SET PhoneNumber='1-161-888-4845' WHERE ClientId='10';
+UPDATE Client SET PhoneNumber='789.552.3165' WHERE ClientId='11';
+UPDATE Client SET PhoneNumber='251.251.9671x156' WHERE ClientId='12';
+
+-- Inserting 9 Services --
+INSERT INTO Service (FacilityId) VALUES ('11');
+INSERT INTO Service (FacilityId) VALUES ('12');
+INSERT INTO Service (FacilityId) VALUES ('13');
+INSERT INTO Service (FacilityId) VALUES ('21');
+INSERT INTO Service (FacilityId) VALUES ('22');
+INSERT INTO Service (FacilityId) VALUES ('31');
+INSERT INTO Service (FacilityId) VALUES ('32');
+INSERT INTO Service (FacilityId) VALUES ('41');
+INSERT INTO Service (FacilityId) VALUES ('42');
+
+-- Inserting 6 ClientServices --
+INSERT INTO ClientService (FacilityId,SiteId,FacilityName,HoursOfOperation,EligibilityCondition) VALUES ('31','1','pantry1','8:00AM-6:00PM Monday-Friday','Drivers License/PhotoID');
+INSERT INTO ClientService (FacilityId,SiteId,FacilityName,HoursOfOperation,EligibilityCondition) VALUES ('41','2','soup2','8:00AM-6:00PM Monday-Friday','Drivers License/PhotoID');
+INSERT INTO ClientService (FacilityId,SiteId,FacilityName,HoursOfOperation,EligibilityCondition) VALUES ('21','2','shelter2','9:00PM-9:00AM Sunday-Saturday','PhotoID');
+INSERT INTO ClientService (FacilityId,SiteId,FacilityName,HoursOfOperation,EligibilityCondition) VALUES ('32','3','pantry3','8:00AM-6:00PM Monday-Friday','Drivers License/PhotoID');
+INSERT INTO ClientService (FacilityId,SiteId,FacilityName,HoursOfOperation,EligibilityCondition) VALUES ('42','3','soup3','8:00AM-6:00PM Monday-Friday','Drivers License/PhotoID');
+INSERT INTO ClientService (FacilityId,SiteId,FacilityName,HoursOfOperation,EligibilityCondition) VALUES ('22','3','shelter3','9:00PM-9:00AM Sunday-Saturday','PhotoID');
+
+-- Inserting 3 FoodBanks --
+INSERT INTO FoodBank (FacilityId) VALUES ('11');
+INSERT INTO FoodBank (FacilityId) VALUES ('12');
+INSERT INTO FoodBank (FacilityId) VALUES ('13');
+
+-- Inserting 2 Shelters --
+INSERT INTO Shelter (FacilityId,BunkCountMale,BunkCountFemale,BunkCountMixed) VALUES ('21','2','2','2');
+INSERT INTO Shelter (FacilityId,BunkCountMale,BunkCountFemale,BunkCountMixed) VALUES ('22','2','2','2');
+
+-- Inserting 2 FoodPantries --
+INSERT INTO FoodPantry (FacilityId) VALUES ('31');
+INSERT INTO FoodPantry (FacilityId) VALUES ('32');
+
+-- Inserting 2 SoupKitchens --
+INSERT INTO SoupKitchen (FacilityId,SeatAvail,SeatTotal) VALUES ('31','30','100');
+INSERT INTO SoupKitchen (FacilityId,SeatAvail,SeatTotal) VALUES ('32','10','100');
+
+-- Inserting 3 SiteToServices --
+INSERT INTO SiteToService (FacilityId,SiteId) VALUES ('11','1');
+INSERT INTO SiteToService (FacilityId,SiteId) VALUES ('12','2');
+INSERT INTO SiteToService (FacilityId,SiteId) VALUES ('13','3');
+
+-- Inserting Items --
+INSERT INTO Item (ItemId,Name,ExpDate,StorageType,Category,SubCategory) VALUES ('1','kale',NOW()+INTERVAL 10 DAY,'Refrigerated','Food','Vegetables');
+INSERT INTO Item (ItemId,Name,ExpDate,StorageType,Category,SubCategory) VALUES ('2','spinach',NOW()+INTERVAL 10 DAY,'Refrigerated','Food','Vegetables');
+INSERT INTO Item (ItemId,Name,ExpDate,StorageType,Category,SubCategory) VALUES ('3','peanuts',NOW()+INTERVAL 10 DAY,'Drygoods','Food','Nuts/grains/beans');
+INSERT INTO Item (ItemId,Name,ExpDate,StorageType,Category,SubCategory) VALUES ('4','almonds',NOW()+INTERVAL 10 DAY,'Drygoods','Food','Nuts/grains/beans');
+INSERT INTO Item (ItemId,Name,ExpDate,StorageType,Category,SubCategory) VALUES ('5','pepsi',NOW()+INTERVAL 10 DAY,'Refrigerated','Food','Juice/drink');
+INSERT INTO Item (ItemId,Name,ExpDate,StorageType,Category,SubCategory) VALUES ('6','club soda',NOW()+INTERVAL 10 DAY,'Refrigerated','Food','Juice/drink');
+INSERT INTO Item (ItemId,Name,ExpDate,StorageType,Category,SubCategory) VALUES ('7','beef',NOW()+INTERVAL 10 DAY,'Frozen','Food','Meat/seafood');
+INSERT INTO Item (ItemId,Name,ExpDate,StorageType,Category,SubCategory) VALUES ('8','pork',NOW()+INTERVAL 10 DAY,'Frozen','Food','Meat/seafood');
+INSERT INTO Item (ItemId,Name,ExpDate,StorageType,Category,SubCategory) VALUES ('9','chaddar cheese',NOW()+INTERVAL 10 DAY,'Refrigerated','Food','Dairy/eggs');
+INSERT INTO Item (ItemId,Name,ExpDate,StorageType,Category,SubCategory) VALUES ('10','swiss cheese',NOW()+INTERVAL 10 DAY,'Refrigerated','Food','Dairy/eggs');
+INSERT INTO Item (ItemId,Name,ExpDate,StorageType,Category,SubCategory) VALUES ('11','toothbrush',NOW()+INTERVAL 10 DAY,'Drygoods','Supply','Personal hygiene'),
+                                                                               ('12','toothpaste',NOW()+INTERVAL 10 DAY,'Drygoods','Supply','Personal hygiene'),
+																			   ('13','shampoo',NOW()+INTERVAL 10 DAY,'Drygoods','Supply','Personal hygiene'),
+																			   ('14','babywipes',NOW()+INTERVAL 10 DAY,'Drygoods','Supply','Personal hygiene'),
+																			   ('15','tshirt',NOW()+INTERVAL 10 DAY,'Drygoods','Supply','Personal hygiene'),
+																			   ('16','pants',NOW()+INTERVAL 10 DAY,'Drygoods','Supply','Clothing'),
+                                                                               ('17','shirts',NOW()+INTERVAL 10 DAY,'Drygoods','Supply','Clothing'),
+																			   ('18','jeans',NOW()+INTERVAL 10 DAY,'Drygoods','Supply','Clothing'),
+																			   ('19','underwear',NOW()+INTERVAL 10 DAY,'Drygoods','Supply','Clothing'),
+																			   ('20','coats',NOW()+INTERVAL 10 DAY,'Drygoods','Supply','Clothing'),
+																			  ('21','beets',NOW()+INTERVAL 10 DAY,'Refrigerated','Food','Vegetables'),
+                                                                               ('22','carrots',NOW()+INTERVAL 10 DAY,'Refrigerated','Food','Vegetables'),
+																			   ('23','rice',NOW()+INTERVAL 10 DAY,'Drygoods','Food','Nuts/grains/beans'),
+																			   ('24','oats',NOW()+INTERVAL 10 DAY,'Drygoods','Food','Nuts/grains/beanse'),
+																			   ('25','A1 sauce',NOW()+INTERVAL 10 DAY,'Drygoods','Food','Sauce/condiments'),
+																			   ('26','catchups',NOW()+INTERVAL 10 DAY,'Drygoods','Food','Sauce/condiments'),
+                                                                               ('27','orange juice',NOW()+INTERVAL 10 DAY,'Refrigerated','Food','Juice/drink'),
+																			   ('28','apple juice',NOW()+INTERVAL 10 DAY,'Refrigerated','Food','Juice/drink'),
+																			   ('29','shrimp',NOW()+INTERVAL 10 DAY,'Frozen','Food','Meat/seafood'),
+																			   ('30','salmon',NOW()+INTERVAL 10 DAY,'Frozen','Food','Meat/seafood'),
+																			   ('31','eggs',NOW()+INTERVAL 10 DAY,'Refrigerated','Food','Dairy/eggs'),
+                                                                               ('32','egg white',NOW()+INTERVAL 10 DAY,'Refrigerated','Food','Dairy/eggs'),
+																			   ('33','tent',NOW()+INTERVAL 10 DAY,'Drygoods','Supply','Shelter'),
+																			   ('34','sleeping bags',NOW()+INTERVAL 10 DAY,'Drygoods','Supply','Shelter'),
+																			   ('35','blankets',NOW()+INTERVAL 10 DAY,'Drygoods','Supply','Shelter'),
+																			   ('36','umbrella',NOW()+INTERVAL 10 DAY,'Drygoods','Supply','Shelter'),
+                                                                               ('37','rain coats',NOW()+INTERVAL 10 DAY,'Drygoods','Supply','Shelter'),
+																			   ('38','toilet paper',NOW()+INTERVAL 10 DAY,'Drygoods','Supply','Other'),
+																			   ('39','pet food',NOW()+INTERVAL 10 DAY,'Drygoods','Supply','Other'),
+																			   ('40','batteries',NOW()+INTERVAL 10 DAY,'Drygoods','Supply','Other'),
+                                                                               ('41','pencils',NOW()+INTERVAL 10 DAY,'Drygoods','Supply','Other'),
+																			   ('42','Note pads',NOW()+INTERVAL 10 DAY,'Drygoods','Supply','Other'),
+																			   ('43','chicken breats',NOW()-INTERVAL 10 DAY,'Frozen','Food','Meat/seafood'),
+																			   ('44','chicken legs',NOW()-INTERVAL 10 DAY,'Frozen','Food','Meat/seafood'),
+																			   ('45','whole milk',NOW()-INTERVAL 10 DAY,'Refrigerated','Food','Dairy/eggs'),
+																			   ('46','non-fat milk',NOW()-INTERVAL 10 DAY,'Refrigerated','Food','Dairy/eggs');
+																			   
+-- Inserting FoodBankToItem --
+INSERT INTO FoodBankToItem (FacilityId,ItemId,availableQuantity)
+VALUES ('11','1','10'),
+       ('11','2','10'),
+	   ('11','3','10'),
+	   ('11','4','10'),
+	   ('11','5','10'),
+	   ('11','6','10'),
+	   ('11','7','10'),
+	   ('11','8','10'),
+	   ('11','9','10'),
+	   ('11','10','10'),
+	   ('11','11','10'),
+	   ('11','12','10'),
+	   ('11','13','10'),
+	   ('11','14','10'),
+	   ('11','15','10'),
+	   ('11','16','10'),
+	   ('11','17','10'),
+	   ('11','18','10'),
+	   ('11','19','10'),
+	   ('11','20','10'),
+	   ('12','21','20'),
+       ('12','22','20'),
+	   ('12','23','20'),
+	   ('12','24','20'),
+	   ('12','25','20'),
+	   ('12','26','20'),
+	   ('12','27','20'),
+	   ('12','28','20'),
+	   ('12','29','20'),
+	   ('12','30','20'),
+	   ('12','31','20'),
+	   ('12','32','20'),
+	   ('12','33','20'),
+	   ('12','34','20'),
+	   ('12','35','20'),
+	   ('12','36','20'),
+	   ('12','37','20'),
+	   ('12','38','20'),
+	   ('12','39','20'),
+	   ('12','40','20'),
+	   ('12','41','10'),
+	   ('12','42','10'),
+	   ('13','43','10'),
+	   ('13','44','10'),
+	   ('13','45','10'),
+	   ('13','46','10');
+	   
+-- Inserting Request--	 
+insert into Request (RequestID,Username,FacilityId,ItemId,Status,QuantityRequested,QuantityFulfilled)
+values
+(1,'emp1',12,21,'pending',20,0),
+(2,'emp1',12,22,'pending',20,0),
+(3,'emp1',12,23,'pending',30,0),
+(4,'emp1',13,43,'pending',20,0),
+(5,'emp1',13,44,'pending',30,0),
+(6,'emp2',11,1,'pending',30,0),
+(7,'emp2',11,2,'pending',30,0),																			   
+(8,'emp2',11,3,'pending',30,0),
+(9,'emp2',11,4,'pending',30,0),
+(10,'emp2',13,45,'pending',20,0),
+(11,'emp2',13,46,'pending',30,0),
+(12,'emp2',11,11,'pending',30,0),
+(13,'emp2',11,12,'pending',30,0),																			   
+(14,'emp2',11,13,'pending',30,0),
+(15,'emp3',11,5,'pending',30,0),
+(16,'emp3',11,6,'pending',30,0),																			   
+(17,'emp3',11,7,'pending',30,0),
+(18,'emp3',11,8,'pending',30,0),
+(19,'emp3',12,24,'pending',20,0),
+(20,'emp3',12,25,'pending',30,0),
+(21,'emp3',11,11,'pending',30,0),
+(22,'emp3',11,12,'pending',30,0),																			   
+(23,'emp3',11,13,'pending',30,0),
+(24,'emp3',12,33,'pending',30,0),
+(25,'emp3',12,34,'pending',30,0),																			   
+(26,'emp3',12,35,'pending',30,0),
+(27,'emp3',11,11,'pending',30,0),
+(28,'emp3',11,12,'pending',30,0),																			   
+(29,'emp3',11,13,'pending',30,0),
+(30,'emp3',11,11,'pending',30,0),
+(31,'emp1',12,21,'closed',30,30),																			   
+(32,'emp1',12,22,'closed',30,30),
+(33,'emp2',11,1,'closed',20,20),																			   
+(34,'emp2',11,2,'closed',20,20),
+(35,'emp3',11,5,'closed',10,10),
+(36,'emp3',11,6,'closed',10,10),
+(37,'vol1',12,21,'pending',20,0),
+(38,'vol1',12,22,'pending',20,0),
+(39,'vol1',12,23,'pending',30,0),
+(40,'vol1',13,43,'pending',20,0),
+(41,'vol1',13,44,'pending',30,0),
+(42,'vol2',11,1,'pending',30,0),
+(43,'vol2',11,2,'pending',30,0),																			   
+(44,'vol2',11,3,'pending',30,0),
+(45,'vol2',11,4,'pending',30,0),
+(46,'vol2',13,45,'pending',20,0),
+(47,'vol2',13,46,'pending',30,0),
+(48,'vol2',11,11,'pending',30,0),
+(49,'vol2',11,12,'pending',30,0),																			   
+(50,'vol2',11,13,'pending',30,0),
+(51,'vol3',11,5,'pending',30,0),
+(52,'vol3',11,6,'pending',30,0),																			   
+(53,'vol3',11,7,'pending',30,0),
+(54,'vol3',11,8,'pending',30,0),
+(55,'vol3',12,24,'pending',20,0),
+(56,'vol3',12,25,'pending',30,0),
+(57,'vol3',11,11,'pending',30,0),
+(58,'vol3',11,12,'pending',30,0),																			   
+(59,'vol3',11,13,'pending',30,0),
+(60,'vol3',12,33,'pending',30,0),
+(61,'vol3',12,34,'pending',30,0),																			   
+(62,'vol3',12,35,'pending',30,0),
+(63,'vol3',11,11,'pending',30,0),
+(64,'vol3',11,12,'pending',30,0),																			   
+(65,'vol3',11,13,'pending',30,0),
+(66,'vol3',11,11,'pending',30,0),
+(67,'vol1',12,21,'closed',30,30),																			   
+(68,'vol1',12,22,'closed',30,30),
+(69,'vol2',11,1,'closed',20,20),																			   
+(70,'vol2',11,2,'closed',20,20),
+(71,'vol3',11,5,'closed',10,10),
+(72,'vol3',11,6,'closed',10,10);
