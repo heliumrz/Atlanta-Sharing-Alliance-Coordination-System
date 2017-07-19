0faaaa4fd677c5ebb07ec5741605917f35256417
Azade Sanjari <azades@users.noreply.github.com>
2017-07-19 01:50:47 -0700

    moved functions to their own pages

--- Phase3/src/create_service.php
+++ Phase3/src/create_service.php
@@ -1,6 +1,105 @@
 <?php
    include 'lib.php';
    
+   // input field for facilityName
+   function displayClientServiceInputFields() {
+       echo ' <p>
+          <label>Facility Name: </label>
+          <input id="facilityName" name="facilityName" type="text" value="' . $EMPTY_STRING . '"/>
+          </p>
+   	   <p>
+          <label>Eligibility Condition: </label>
+          <input id="EligibilityCondition" name="EligibilityCondition" type="text" value="' . $EMPTY_STRING . '"/>
+          </p>
+   	   <p>
+          <label>Hours Of Operation: </label>
+          <input id="HoursOfOperation" name="HoursOfOperation" type="text" value="' . $EMPTY_STRING . '"/>
+          </p> ';	
+   }
+
+   function displaySoupKitchenInputFields() {
+       echo ' <p>
+          <label>Seat Available: </label>
+          <input id="SeatAvail" name="SeatAvail" type="text" value="' . $EMPTY_STRING . '"/>
+          <label>Seat Total: </label>
+          <input id="SeatTotal" name="SeatTotal" type="text" value="' . $EMPTY_STRING . '"/>
+          </p> ';	
+   }
+
+   function displayShelterInputFields() {
+       echo ' <p>
+          <label>Bunk Type: </label>
+          <input id="BunkType" name="BunkType" type="text" value="' . $EMPTY_STRING . '"/>
+          </p>
+   	   <p>
+          <label>Bunk Count (Male): </label>
+          <input id="BunkCountMale" name="BunkCountMale" type="text" value="' . $EMPTY_STRING . '"/>
+          </p>
+   	   <p>
+          <label>Bunk Count (Female):</label>
+          <input id="BunkCountFemale" name="BunkCountFemale" type="text" value="' . $EMPTY_STRING . '"/>
+          </p> 
+          <label>Bunk Count (Mixed):</label>
+          <input id="BunkCountMixed" name="BunkCountMixed" type="text" value="' . $EMPTY_STRING . '"/>
+          </p> ';	
+   }
+
+   function retrieveAllSiteNames() {
+      $sql = "SELECT ShortName, SiteId FROM Site";
+      $result = executeSql($sql);
+      return $result;
+   }
+
+   function displaySiteNamesOptions() {
+      $result = retrieveAllSiteNames();
+      $str = "<select id='Site' name='Site'>";
+      while($row = $result->fetch_assoc()) {
+         $str = $str . "
+            <option value='" . $row['SiteId'] . "'>" . $row['ShortName'] . "</option>";
+      }
+      $str = $str . " </select>";
+      echo $str;
+   }
+   # adds a facility and returns the id of the new row
+   function addFacilityToServiceTable() {
+   	$sql = "INSERT INTO Service (FacilityId) VALUES (NULL)";
+       return insertSql($sql);
+   }
+
+   function addFacilityToClientServiceTable($FacilityId, $SiteId, $FacilityName, $EligibilityCondition, $HoursOfOperation) {
+   	$sql = "INSERT INTO ClilentService (FacilityId, SiteId, FacilityName, EligibilityCondition, HoursOfOperation) VALUES (". 
+   		$FacilityId .",'". $SiteId ."','". $FacilityName ."','". $EligibilityCondition ."','". $HoursOfOperation ."')";
+       return insertSql($sql);
+   }
+
+   function addFacilityToSoupKitchen($FacilityId, $SeatAvail, $SeatTotal) {
+   	$sql = "INSERT INTO SoupKitchen (FacilityId, SeatAvail, SeatTotal) VALUES ('" . 
+   		$FacilityId . "','". $SeatAvail . "','". $SeatTotal . "')";
+   	return insertSql($sql);
+   }
+
+   function addFacilityToFoodBank($FacilityId) {
+   	$sql = "INSERT INTO FoodBank (FacilityId) VALUES ('" . $FacilityId . "')";
+   	return insertSql($sql);
+   }
+
+   function addFacilityToFoodPantry($FacilityId) {
+   	$sql = "INSERT INTO FoodPantry (FacilityId) VALUES ('" . $FacilityId . "')";
+   	return insertSql($sql);
+   }
+
+   function addFacilityToShelter($FacilityId) {
+   	$sql = 	$sql = "INSERT INTO Shelter (FacilityId, BunkType, BunkCountMale, BunkCountFemale, BunkCountMixed) VALUES ('" . 
+   		$FacilityId . "','". $BunkType . "','". $BunkCountMale . "','". $BunkCountFemale . "','". $BunkCountMixed . "')";
+   	return insertSql($sql);
+   }
+
+   function addToSiteToServiceTable($FacilityId, $SiteId) {
+   	$sql = "INSERT INTO SiteToService (FacilityId, Siteid, SeatTotal) VALUES ('" . 
+   		$FacilityId . "','". $SiteId . "')";
+   	return insertSql($sql);
+   }
+   
    $svc = [
        "soupkitchen" => "Soup Kitchen",
        "foodpantry" => "Food Pantry",
@@ -39,31 +138,6 @@
 	} else {
 		//do nothing.
 	}	
-   // // Handle data update logic
-   // if (isset($_POST['updateClient']) && !empty($clientId) && !empty($username)) {
-   //    updateClientData($clientId,$username,$_POST);
-   // }
-   //
-   // // Go to Client Check-In
-   // if (isset($_POST['checkinClient']) && !empty($clientId) && !empty($username)) {
-   //    goToClientCheckin(true);
-   // }
-   //
-   // if (!empty($clientId)) {
-   //    $result = retrieveClientFromId($clientId);
-   //
-   //    if ($result->num_rows > 0) {
-   //       // Client exists, pull back data and process.
-   //       $clientRow = $result->fetch_assoc();
-   //
-   //       // Retrieve history
-   //       $clientModificationHistory = retrieveClientModificationHistory($clientId);
-   //       $clientServiceUsageHistory = retrieveClientServiceUsageHistory($clientId);
-   //    } else {
-   //       // If no client exist, go to Client Search screen
-   //       goToClientSearch(true);
-   //    }
-   // }
 ?>
 <html>
    <head>
@@ -73,20 +147,6 @@
       ?>
       <script>
          <?php displayJavascriptLib();?>
-         
-		  // function validateInput() {
-//             if (validateField("firstName") && validateField("lastName") && validateField("description") && validateValidCharacter("phoneNumber")) {
-//                if (validateDataUpdated()) {
-//                   return true;
-//                } else {
-//                   alert(clientNoDataUpdated);
-//                   return false;
-//                }
-//             } else {
-//                alert(clientRequiredField);
-//                return false;
-//             }
-//          }
       </script>
    </head>
    <body>
