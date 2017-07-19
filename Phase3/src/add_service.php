5f46e6f354145ba73d4b5af80a705c18e17aa737
Azade Sanjari <azades@users.noreply.github.com>
2017-07-18 21:23:30 -0700

    modify service

---
+++ Phase3/src/add_service.php
@@ -0,0 +1,74 @@
+<?php
+include 'lib.php';
+
+$pageTitle = "Add New Service";
+
+session_start();
+$result = null;
+$insertSql = null;
+$clientExists = false;
+
+logout(isset($_POST['logout']));
+goToUserHome(isset($_POST['userHome']));   
+goToClientSearch(isset($_POST['cancel']));
+
+if (isset($_POST['save']) && $_POST['serviceType'] != '0' ) {
+
+	$serviceType = $_POST['serviceType'];
+	$_SESSION["serviceType"] = $serviceType;
+	header("Location: ./create_service.php");
+	echo "something is messed up!"
+	exit;
+//	$insertSql = "INSERT INTO request (Username, FacilityId, ItemId, QuantityRequested, Status, QuantityFulfilled) " .
+  //                 "VALUES ('" . $userName . "','" . $facilityId . "','"  . $itemId . "', '" . $quantityRequested . "','Pending','0' )";
+	  
+	  // $insertSql = "INSERT INTO request (NULL, '" . $userName . "','" . $facilityId . "','"  . $itemId . "','', '" . $quantityRequested . "', '' )";
+	  
+      // $requestId = insertSql($insertSql);
+		 //       if ($requestId > 0) {
+		 //          $_SESSION["requestId"] = $requestId;
+		 //          // header("Location: /request_detail.php");
+		 // echo "Request submitted with id ". $requestId;
+		 //
+		 //          exit;
+		 //       } else {
+		 //          echo "Error: " . $insertSql . "<br>" .$requestId;
+		 //       }  
+} 
+
+?>
+<html>
+   <head>
+      <title><?php displayText($pageTitle);?></title>
+   </head>
+   <body>
+      <form action="./add_service.php" method="post">
+         <div>
+            <div style="float: left"><strong><?php displayText($pageTitle);?></strong>
+            </div>
+            <?php 
+               displayLogout();
+               displayUserHome();
+            ?>
+         </div>
+         <br>
+         <div>
+            <p>
+               <label>
+                  <strong>Select the type of service</strong>
+               </label> 
+			   <select id="serviceType" name="serviceType">                      
+			     <option value="0">--Select Service--</option>
+			     <option value="foodbank">Food Bank</option>
+				 <option value="foodpantry">Food Pantry</option>
+			     <option value="soupkitchen">Soup Kitchen</option>
+			     <option value="shelter">Shelter</option>
+			   </select>
+            </p>
+            <p>
+               <button name="save" type="submit">Next</button>
+            </p>
+         </div>         
+	 </form>
+	</body>
+</html>
---
+++ Phase3/src/create_service.php
@@ -0,0 +1,116 @@
+<?php
+   include 'lib.php';
+   
+   $pageTitle = "Create a new service";
+  
+   session_start();
+   $currentClient = null;
+   $clientRow = null;
+   $clientModificationHistory = null;
+   $clientServiceUsageHistory = null;
+   
+   // Ensure session is valid. If not, go to login page.
+   checkValidSession();
+   
+   // Inlude in all pages
+   logout(isset($_POST['formAction']) && ($_POST['formAction'] == 'logout'));
+   goToUserHome(isset($_POST['formAction']) && ($_POST['formAction'] == 'userHome'));
+   goToClientSearch(isset($_POST['clientSearch']));
+
+   $serviceType = $_SESSION["serviceType"];
+
+   // // Handle data update logic
+   // if (isset($_POST['updateClient']) && !empty($clientId) && !empty($username)) {
+   //    updateClientData($clientId,$username,$_POST);
+   // }
+   //
+   // // Go to Client Check-In
+   // if (isset($_POST['checkinClient']) && !empty($clientId) && !empty($username)) {
+   //    goToClientCheckin(true);
+   // }
+   //
+   // if (!empty($clientId)) {
+   //    $result = retrieveClientFromId($clientId);
+   //
+   //    if ($result->num_rows > 0) {
+   //       // Client exists, pull back data and process.
+   //       $clientRow = $result->fetch_assoc();
+   //
+   //       // Retrieve history
+   //       $clientModificationHistory = retrieveClientModificationHistory($clientId);
+   //       $clientServiceUsageHistory = retrieveClientServiceUsageHistory($clientId);
+   //    } else {
+   //       // If no client exist, go to Client Search screen
+   //       goToClientSearch(true);
+   //    }
+   // }
+?>
+<html>
+   <head>
+      <?php 
+         displayTitle($pageTitle);
+         displayCss();
+      ?>
+      <script>
+         <?php displayJavascriptLib();?>
+         
+         // Determine whether any data element was changed
+         function validateDataUpdated() {
+            var currentFirstName = document.getElementById("currentFirstName").value;
+            var currentLastName = document.getElementById("currentLastName").value;
+            var currentDescription = document.getElementById("currentDescription").value;
+            var currentPhoneNumber = document.getElementById("currentPhoneNumber").value;
+            
+            var firstName = document.getElementById("firstName").value;
+            var lastName = document.getElementById("lastName").value;
+            var description = document.getElementById("description").value;
+            var phoneNumber = document.getElementById("phoneNumber").value;
+            
+            if ((currentFirstName != firstName) || (currentLastName != lastName) || (currentDescription != description) || (currentPhoneNumber != phoneNumber)) {
+               return true;
+            }
+            return false;
+         }
+         
+         function validateInput() {
+            if (validateField("firstName") && validateField("lastName") && validateField("description") && validateValidCharacter("phoneNumber")) {
+               if (validateDataUpdated()) {
+                  return true;
+               } else {
+                  alert(clientNoDataUpdated);
+                  return false;
+               }
+            } else {
+               alert(clientRequiredField);
+               return false;
+            }
+         }
+      </script>
+   </head>
+   <body>
+      <!-- <?php displayFormHeader($MAIN_FORM,$CLIENT_DETAIL_URL); ?> -->
+         <div>
+            <?php displayPageHeading($pageTitle); ?>            
+            <?php 
+               displayLogout();
+               displayUserHome();
+            ?>
+         </div>
+         <br>
+		 <?php echo $serviceType; ?>
+         <div>
+            <!-- <?php displayClientDataField($clientRow);?> -->
+            <p>
+               <!-- <?php displayUpdateClientSubmitButton(); ?>
+               <?php displayCheckinClientSubmitButton(); ?>
+               <?php displayClientSearchSubmitButton(); ?>
+               <?php displayHiddenField(); ?> -->               
+            </p>
+         </div>
+         <!-- <?php
+           displayClientModificationHistory($clientModificationHistory);
+           displayClientServiceUsageHistory($clientServiceUsageHistory);
+         ?> -->
+</form>
+</body>
+</html>
