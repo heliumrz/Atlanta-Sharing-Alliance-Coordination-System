7a5d1ba90d60eae0cede5111e4de4e24a541e842
Azade Sanjari <azades@users.noreply.github.com>
2017-07-19 01:33:43 -0700

    new services functions

--- Phase3/src/lib.php
+++ Phase3/src/lib.php
@@ -751,4 +751,107 @@ function addClientServiceUsage($clientId,$siteId,$facilityId,$username,$descript
    // echo "addClientServiceUsage sql: " . $sql;
    return insertSql($sql);
 }
+
+// input field for facilityName
+function displayClientServiceInputFields() {
+    echo ' <p>
+       <label>Facility Name: </label>
+       <input id="facilityName" name="facilityName" type="text" value="' . $EMPTY_STRING . '"/>
+       </p>
+	   <p>
+       <label>Eligibility Condition: </label>
+       <input id="EligibilityCondition" name="EligibilityCondition" type="text" value="' . $EMPTY_STRING . '"/>
+       </p>
+	   <p>
+       <label>Hours Of Operation: </label>
+       <input id="HoursOfOperation" name="HoursOfOperation" type="text" value="' . $EMPTY_STRING . '"/>
+       </p> ';	
+}
+
+function displaySoupKitchenInputFields() {
+    echo ' <p>
+       <label>Seat Available: </label>
+       <input id="SeatAvail" name="SeatAvail" type="text" value="' . $EMPTY_STRING . '"/>
+       <label>Seat Total: </label>
+       <input id="SeatTotal" name="SeatTotal" type="text" value="' . $EMPTY_STRING . '"/>
+       </p> ';	
+}
+
+function displayShelterInputFields() {
+    echo ' <p>
+       <label>Bunk Type: </label>
+       <input id="BunkType" name="BunkType" type="text" value="' . $EMPTY_STRING . '"/>
+       </p>
+	   <p>
+       <label>Bunk Count (Male): </label>
+       <input id="BunkCountMale" name="BunkCountMale" type="text" value="' . $EMPTY_STRING . '"/>
+       </p>
+	   <p>
+       <label>Bunk Count (Female):</label>
+       <input id="BunkCountFemale" name="BunkCountFemale" type="text" value="' . $EMPTY_STRING . '"/>
+       </p> 
+       <label>Bunk Count (Mixed):</label>
+       <input id="BunkCountMixed" name="BunkCountMixed" type="text" value="' . $EMPTY_STRING . '"/>
+       </p> ';	
+}
+
+function retrieveAllSiteNames() {
+   $sql = "SELECT ShortName, SiteId FROM Site";
+   $result = executeSql($sql);
+   return $result;
+}
+
+function displaySiteNamesOptions() {
+   $result = retrieveAllSiteNames();
+   $str = "<select id='Site' name='Site'>";
+   
+   while($row = $result->fetch_assoc()) {
+      $str = $str . "
+         <option value='" . $row['SiteId'] . "'>" . $row['ShortName'] . "</option>";
+   }
+   $str = $str . "
+      </select>";
+   echo $str;
+}
+
+# adds a facility and returns the id of the new row
+function addFacilityToServiceTable() {
+	$sql = "INSERT INTO Service (FacilityId) VALUES (NULL)";
+    return insertSql($sql);
+}
+
+function addFacilityToClientServiceTable($FacilityId, $SiteId, $FacilityName, $EligibilityCondition, $HoursOfOperation) {
+	$sql = "INSERT INTO ClilentService (FacilityId, SiteId, FacilityName, EligibilityCondition, HoursOfOperation) VALUES (". 
+		$FacilityId .",'". $SiteId ."','". $FacilityName ."','". $EligibilityCondition ."','". $HoursOfOperation ."')";
+    return insertSql($sql);
+}
+
+function addFacilityToSoupKitchen($FacilityId, $SeatAvail, $SeatTotal) {
+	$sql = "INSERT INTO SoupKitchen (FacilityId, SeatAvail, SeatTotal) VALUES ('" . 
+		$FacilityId . "','". $SeatAvail . "','". $SeatTotal . "')";
+	return insertSql($sql);
+}
+
+function addFacilityToFoodBank($FacilityId) {
+	$sql = "INSERT INTO FoodBank (FacilityId) VALUES ('" . $FacilityId . "')";
+	return insertSql($sql);
+}
+
+function addFacilityToFoodPantry($FacilityId) {
+	$sql = "INSERT INTO FoodPantry (FacilityId) VALUES ('" . $FacilityId . "')";
+	return insertSql($sql);
+}
+
+function addFacilityToShelter($FacilityId) {
+	$sql = 	$sql = "INSERT INTO Shelter (FacilityId, BunkType, BunkCountMale, BunkCountFemale, BunkCountMixed) VALUES ('" . 
+		$FacilityId . "','". $BunkType . "','". $BunkCountMale . "','". $BunkCountFemale . "','". $BunkCountMixed . "')";
+	return insertSql($sql);
+}
+
+function addToSiteToServiceTable($FacilityId, $SiteId) {
+	$sql = "INSERT INTO SiteToService (FacilityId, Siteid, SeatTotal) VALUES ('" . 
+		$FacilityId . "','". $SiteId . "')";
+	return insertSql($sql);
+}
+
 ?>
