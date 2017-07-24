20be1ee9adaf3e786febe9aeef824f364a722909
Azade Sanjari <azades@users.noreply.github.com>
2017-07-24 00:41:46 -0700

    item request + style changes

--- Phase3/src/request_item.php
+++ Phase3/src/request_item.php
@@ -4,40 +4,56 @@ include 'lib.php';
 $pageTitle = "Add New Request";
 
 session_start();
-$result = null;
-$insertSql = null;
-$clientExists = false;
 
 logout(isset($_POST['logout']));
 goToUserHome(isset($_POST['userHome']));   
 goToClientSearch(isset($_POST['cancel']));
 
-//if (isset($_POST['save']) && !empty($_POST['userName']) && !empty($_POST['facilityId']) && !empty($_POST['itemId']) && !empty($_POST['quantityRequested']) ) {
-if (isset($_POST['save']) && !empty($_GET['facilityId']) && !empty($_GET['itemId'])) ) {
+function getItemwithItemId($itemId){
+    $sql = "SELECT * FROM item where ItemId=".$itemId;
+    return executeSql($sql);
+}
+$displayItemToRequest = false;
+
+if (isset($_POST['request']) && isset($_POST['facilityId']) && isset($_POST['itemId'])) {
+    //just ask for quantity
+    $itemId = $_POST['itemId'];
+    $FacilityId = $_POST['facilityId'];
+    $facilityName = $_POST['facilityName'];
+    $result = getItemwithItemId($itemId);
+    while ($item = $result->fetch_assoc()){
+        $itemName = $item['Name'];
+        $storageType = $item['StorageType'];
+        $expDate = $item['ExpDate'];
+    }
+    $displayItemToRequest = true;
+}
+
+if (isset($_POST['save']) ) {
    $userName = $_POST['userName'];
    $facilityId = $_POST['facilityId'];
    $itemId = $_POST['itemId'];
    $quantityRequested = $_POST['quantityRequested'];
    
-
-
-	  $insertSql = "INSERT INTO request (Username, FacilityId, ItemId,  Status, QuantityRequested,, QuantityFulfilled) " .
+    $insertSql = "INSERT INTO request (Username, FacilityId, ItemId,  Status, QuantityRequested, QuantityFulfilled) " .
                    "VALUES ('" . $userName . "','" . $facilityId . "','"  . $itemId . "', 'Pending', '" . $quantityRequested . "','0' )";
-      $requestId = insertSql($insertSql);
-      if ($requestId > 0) {
-         $_SESSION["requestId"] = $requestId;
-         // header("Location: /request_detail.php");
-		 echo "Request submitted with id ". $requestId;
-         exit;
-      }            
+    $requestId = insertSql($insertSql);
+    if ($requestId > 0) {
+         $_SESSION['message'] = "Request submitted with id ". $requestId;
+      } else {
+          $_SESSION['message'] = "There was a problem submitting your request. Try again later.";
+      }
+      $displayResult = true;
 } 
-
 ?>
 <html>
    <head>
-      <title><?php displayText($pageTitle);?></title>
+       <?php 
+          displayTitle($pageTitle);
+          displayCss();
+       ?>
    </head>
-   <body>
+   <?php displayBodyHeading(); ?>
       <form action="/request_item.php" method="post">
          <div>
             <div style="float: left"><strong><?php displayText($pageTitle);?></strong>
@@ -49,40 +65,39 @@ if (isset($_POST['save']) && !empty($_GET['facilityId']) && !empty($_GET['itemId
          </div>
          <br>
          <div>
-            <p>
-               <label>
-                  <strong>User Name</strong>
-               </label> 
-               <input name="userName" required="" type="text" />
-            </p>
-            <p>
-               <label>
-                  <strong>Facility ID</strong>
-               </label> 
-               <input name="facilityId" required="" type="text" />
-            </p>
-            <p>
-               <label>
-                  <strong>Item ID</strong>
-               </label> 
-               <input name="itemId" required="" type="text" />
-            </p>
+                <?php
+                if ($displayItemToRequest) {
+                    echo '
+                        <p>
+                           <label>
+                              <strong>Facility Name: </strong>
+                           </label> 
+                           <strong>'.$facilityName.'<strong/>
+                        </p>
+                    <label>
+                       <strong> Item: </strong>
+                    </label> 
+                    <strong>'.$itemName.'</strong>
+                 </p>
+                    <input name="itemId" value="'. $itemId. '" type="hidden" />
+                    <input name="facilityId" value="'. $FacilityId. '" type="hidden" />
+                    <input name="userName" value="'. $_SESSION['username']. '" type="hidden" />
+                 <p>
+                    <label>
+                       <strong>quantity Requested</strong>
+                    </label> 
+                    <input name="quantityRequested" type="text" />
+                 </p>
             
-            <p>
-               <label>
-                  <strong>quantity Requested</strong>
-               </label> 
-               <input name="quantityRequested" type="text" />
-            </p>
-            
-            <p>
-               <button name="save" type="submit">Submit Request</button>
-               
-            </p>
+                 <p>
+                    <button name="save" type="submit">Submit Request</button>
+                    ';
+                }
+                if ($displayResult) {
+                    echo $_SESSION['message'];
+                }
+                ?>
          </div>
-         
-      </tbody>
-   </table>
 </form>
 </body>
 </html>
